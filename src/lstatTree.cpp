223cd47d93092b8c967c1a5d593cc2317db77d9f
Emyr James <emyrjames1@gmail.com>
2015-02-10 14:23:34 +0000

    adding costs bu gid's

--- src/lstatTree.cpp
+++ src/lstatTree.cpp
@@ -231,18 +231,21 @@ int main(int argc, char **argv) {
         double atime_cost=cost_per_tib_year*tib*atime_years;
         addAttribute(im,"atime_cost",atime_cost);
         addAttribute(im,"atime_cost_by uid_",atime_cost,owner);
+        addAttribute(im,"atime_cost_by gid_",atime_cost,grp);
         addAttribute(im,"atime_cost_by_gid_uid_",atime_cost,grp,owner);
 
         // mtime related
         double mtime_cost=cost_per_tib_year*tib*mtime_years;
         addAttribute(im,"mtime_cost",atime_cost);
         addAttribute(im,"mtime_cost_by uid_",atime_cost,owner);
+        addAttribute(im,"mtime_cost_by gid_",atime_cost,grp);
         addAttribute(im,"mtime_cost_by_gid_uid_",atime_cost,grp,owner);
 
         // ctime related
         double ctime_cost=cost_per_tib_year*tib*ctime_years;
         addAttribute(im,"ctime_cost",ctime_cost);
         addAttribute(im,"ctime_cost_by uid_",ctime_cost,owner);
+        addAttribute(im,"ctime_cost_by gid_",ctime_cost,grp);
         addAttribute(im,"ctime_cost_by_gid_uid_",ctime_cost,grp,owner);
 
         // TODO : file suffix related (bams, vcfs etc)
