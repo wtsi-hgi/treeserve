d66202c0ab44d238f8e15a2f9e5d980242e6bf7e
Joshua C. Randall <jcrandall@alum.mit.edu>
2015-06-03 14:18:53 +0100

    Adds support for NO_PROXY and traps script exit
    Now kills container when script is exited (such as
    by pressing ctrl-c)

--- treeserve-docker.sh
+++ treeserve-docker.sh
@@ -36,8 +36,15 @@ datfile="$(readlink -f ${datfile})"
 groupfile="$(readlink -f ${groupfile})"
 passwdfile="$(readlink -f ${passwdfile})"
 
+export NO_PROXY=/var/run/docker.sock
+
 # make sure image is up to date
 docker pull mercury/treeserve
 
 # launch docker container
-docker run -v ${groupfile}:/etc/group -v ${passwdfile}:/etc/passwd -v ${datfile}:/docker/input.gz -p ${port}:80 mercury/treeserve
+container=$(docker run -d -v ${groupfile}:/etc/group -v ${passwdfile}:/etc/passwd -v ${datfile}:/docker/input.gz -p ${port}:80 mercury/treeserve)
+echo "launched docker container ${container} for ${datfile} on ${port}"
+
+trap "docker kill ${container}" EXIT
+docker logs -f ${container}
+
