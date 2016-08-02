c0e05a4d9f25ccca07b6faf09731be30b45a9c65
Simon Beal <sb48@sanger.ac.uk>
2016-08-02 10:13:46 +0100

    treeserve_web returns json in 'application/json' format treeserve_web now handles errors - returns in a error list

--- treeserve_web.py
+++ treeserve_web.py
@@ -1,16 +1,30 @@
-from flask import Flask, request, redirect, render_template, send_from_directory
-
-import json
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/api")
 def api_call():
-    depth = int(request.args.get("depth", "0"))
-    path = request.args["path"]
+    errors = []
+    if "depth" in request.args:
+        depth = request.args["depth"]
+        try:
+            depth = int(depth, 10)
+        except ValueError:
+            depth = 0
+            errors.append("'depth' wasn't an integer")
+    else:
+        errors.append("Didn't recieve integer parameter 'depth'")
+    if "path" in request.args:
+        path = request.args["path"]
+    else:
+        errors.append("Didn't recieve parameter 'path'")
+    if errors:
+        error_dict = {"errors": errors}
+        return jsonify(error_dict)
+        
     print("API called, path =", path, "depth =",depth)
     output_dict = {}
-    return json.dumps(output_dict)
+    return jsonify(output_dict)
 
 
 if __name__ == '__main__':
