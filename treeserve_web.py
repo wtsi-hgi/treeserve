ca240e65a568786dd26dafe89b5bccbb4cbd1e64
Simon Beal <sb48@sanger.ac.uk>
2016-08-02 10:30:01 +0100

    treeserve_web now uses treeserve_builder to create a tree object also uses tree.to_json() to output json

--- treeserve_web.py
+++ treeserve_web.py
@@ -1,12 +1,34 @@
 from flask import Flask, request, jsonify
 
+from treeserve.tree_builder import TreeBuilder
+
+import glob
+
 app = Flask(__name__)
 
 @app.route("/api")
 def api_call():
+    path, depth, errors = get_path_depth(request.args)
+    if errors:
+        error_dict = {"errors": errors}
+        return jsonify(error_dict)
+        
+    print("API called, path =", path, "depth =",depth)
+
+    sample_list = glob.glob("samples/*.dat.gz")
+    print("Using samples:", sample_list)
+    tree_builder = TreeBuilder()
+    tree = tree_builder.from_lstat(sample_list)
+    print(tree)
+    print("Created tree.")
+    output_dict = {"tree": tree.to_json(path=path, depth=depth)}
+    return jsonify(output_dict)
+
+def get_path_depth(args):
+    path=depth=None
     errors = []
-    if "depth" in request.args:
-        depth = request.args["depth"]
+    if "depth" in args:
+        depth = args["depth"]
         try:
             depth = int(depth, 10)
         except ValueError:
@@ -14,18 +36,11 @@ def api_call():
             errors.append("'depth' wasn't an integer")
     else:
         errors.append("Didn't recieve integer parameter 'depth'")
-    if "path" in request.args:
-        path = request.args["path"]
+    if "path" in args:
+        path = args["path"]
     else:
         errors.append("Didn't recieve parameter 'path'")
-    if errors:
-        error_dict = {"errors": errors}
-        return jsonify(error_dict)
-        
-    print("API called, path =", path, "depth =",depth)
-    output_dict = {}
-    return jsonify(output_dict)
-
+    return path, depth, errors
 
 if __name__ == '__main__':
     app.debug = True
