ed1102dabe7ff24bb36e4a6530df9276d8bde5ce
Josh Holland <anowlcalledjosh@gmail.com>
2016-08-10 16:43:01 +0100

    Remove unnecessary webserver test

--- treeserve/tests/test_webserver.py
+++ treeserve/tests/test_webserver.py
@@ -1,6 +1,5 @@
 import json
 import unittest
-from nose_parameterized import parameterized
 import treeserve_web as web
 
 class WebTestCase(unittest.TestCase):
@@ -22,18 +21,6 @@ class WebTestCase(unittest.TestCase):
         test_depth = 0
         self.assertEqual(self.load_response(path=test_path, depth=test_depth),
                          web.tree.format(path=test_path, depth=test_depth))
-
-    @parameterized.expand([
-        [{}, ["no 'depth'", "no 'path'"]],
-        [{"depth": 0}, ["no 'path'"]],
-        [{"path": "",}, ["no 'depth'"]],
-        [{"path": "", "depth": "NotAnInt"}, ["'depth' not integer"]]
-    ])
-    def test_queries(self, testcase, expected):
-        test = self.load_response(**testcase)
-        for outcome in test["errors"]:
-            self.assertIn(outcome, expected)
-        self.assertEqual(len(expected), len(test["errors"]))
 
     def load_response(self, **data):
         response = self.app.get("/api", query_string = data)
