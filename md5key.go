baf3c75cce53ad942322bf990d96402a59c23285
Joshua Randall <joshua.randall@sanger.ac.uk>
2016-10-04 16:21:30 +0100

    Add childrendb test and add UnmarshalBinary method to Md5Key

--- md5key.go
+++ md5key.go
@@ -7,6 +7,13 @@ func (k *Md5Key) MarshalBinary() (data []byte, err error) {
 	return
 }
 
+func (k *Md5Key) UnmarshalBinary(data []byte) (err error) {
+	md5Key := Md5Key{}
+	copy(md5Key[:], data)
+	k = &md5Key
+	return
+}
+
 func (k *Md5Key) String() string {
 	return string(k[:])
 }
